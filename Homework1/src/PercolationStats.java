8f20e731c3b0e091f064c135684d68afd1ee4255
mccallumsgd <mccallum.sgd@gmail.com>
2017-09-20 01:38:37 -0600

    Homework 1 completed

--- Homework1/src/PercolationStats.java
+++ Homework1/src/PercolationStats.java
@@ -1,46 +1,47 @@
-
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class PercolationStats {
 
     private final double CONFIDENCE_COEFF = 1.96;
     
-    private double T;
+    private int T;
     private double[] thresholds;
+    private double mean, stddev;
     
     
     public PercolationStats(int N, int T) {
         if (N <= 0 || T <= 0) throw new IllegalArgumentException("N and T must be positive integers.");
+        this.T = T;
         thresholds = new double[T];
-        for (int i = 0; i < T; i++) {
-            StdOut.println("SAMPLE: " + i+1);
-            Percolation perc = new Percolation(N);
-            int count = 0;
-            do {
-                StdOut.println("iteration: " + count+1);
-                perc.open(StdRandom.uniform(0, N), StdRandom.uniform(0, N));
-                count++;
-            } while (!perc.percolates());
-            thresholds[i] = (double)count/N;
-        }
+        runSim(N);
     }
     
     public double mean() {
-        return StdStats.mean(thresholds);
+        mean = StdStats.mean(thresholds);
+        return mean;
     }
     
     public double stddev() {
-        return StdStats.stddev(thresholds);
+        stddev =  StdStats.stddev(thresholds);
+        return stddev;
     }
     
     public double confidenceLow() {
-        double mean = mean(),
-               stddev = stddev();
-        return 0;
-        
+        return mean - CONFIDENCE_COEFF*stddev / Math.sqrt(T);
     }
     
     public double confidenceHigh() {
-        return 0;
+        return mean+(CONFIDENCE_COEFF*stddev/Math.sqrt(T));
+    }
+    
+    private void runSim(int N) {
+        for (int i = 0; i < T; i++) {
+            Percolation perc = new Percolation(N);
+            do {
+                perc.open(StdRandom.uniform(0, N), StdRandom.uniform(0, N));
+            } while (!perc.percolates());
+            thresholds[i] = 1.0*perc.numberOfOpenSites()/(N*N);
+        }
     }
 }
--- Homework1/src/Test.java
+++ Homework1/src/Test.java
@@ -1,9 +1,23 @@
+import edu.princeton.cs.algs4.Stopwatch;
 
 public class Test {
-
     public static void main (String[] args) {
-        PercolationStats sts = new PercolationStats(200, 10);
-        StdOut.println("mean: " + sts.mean() + "\n" + 
-                       "stddev: " + sts.stddev());
+        String results = runTest(25, 10) +
+                runTest(50, 20) +
+                runTest(50, 60) +
+                runTest(100, 60) +
+                runTest(200, 60);
+        System.out.println(results);
+    }
+    
+    private static String runTest(int N, int T) {
+        Stopwatch s = new Stopwatch();
+        PercolationStats pstats = new PercolationStats(N, T);
+        double time = s.elapsedTime();
+        return "running time: " + time + "\n" + 
+               "mean: " + pstats.mean() + "\n" + 
+                "stddev: " + pstats.stddev() + "\n" + 
+                "confidencLow: " + pstats.confidenceLow() + "\n" + 
+                "confidenceHigh: " + pstats.confidenceHigh() + "\n";
     }
 }
